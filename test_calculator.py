4eabb005012cd421653984323464a5276490e77c
tracer <eduardo.pradoruiz@gmail.com>
2020-03-21 16:02:52 +0100

    Add unit tests for calculator
    practice unit test

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+"""
+UNit tests for the calculator library
+"""
+
+import calculator
+
+
+class TestCalculator:
+
+	def test_addition(self):
+		assert 4 == calculator.add(2, 2)
+
+	def test_substraction(self):
+		assert 2 == calculator.subtract(4, 2)
